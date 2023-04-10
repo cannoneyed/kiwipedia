c187031aa3247d0430b8e7ae6f74dbd8d2e63d49
Andrew Coenen <andycoenen@andrews-mini.lan>
2023-04-10 17:01:00 -0400

    Add about

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+import styles from './layout.module.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export default function Footer() {
-  return <></>;
+  return (
+    <div className={styles.footer + ' ' + inter.className}>
+      <div className={styles.footerTitle}>🥝 Kiwipedia</div>
+      <Link href="/about">
+        <span className={styles.link}>About</span>
+      </Link>
+    </div>
+  );
 }
